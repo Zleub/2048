36b6baac2781887d056ac7dc146687acc3358b7b
Arnaud Debray <debray.arnaud@gmail.com>
2015-02-28 16:11:55 +0100

    to school

--- main.c
+++ main.c
@@ -6,17 +6,20 @@
 /*   By: Arno <Arno@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/27 20:42:07 by adebray           #+#    #+#             */
-/*   Updated: 2015/02/28 15:23:42 by Arno             ###   ########.fr       */
+/*   Updated: 2015/02/28 15:35:03 by Arno             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <game.h>
 
-void		ft_getch(int **array)
+int		ft_getch(int **array)
 {
 	int c = getch();
 	dprintf(2, "%d ", c);
+	if (c == ESC)
+		return (0);
 	(void)array;
+	return (1);
 }
 
 int			ft_wincheck(void)
@@ -51,14 +54,24 @@ int			main(void)
 	ft_signal();
 
 	array[0][2] = 10;
-	mvwprintw(stdscr, LINES / 2, COLS / 2 - 15, "Welcome to 2048. Press Space.\n");
+	mvwprintw(stdscr, LINES / 2, COLS / 2 - 15, "Welcome to 2048. Press a key.\n");
 	wrefresh(stdscr);
+
+	if (!(ft_getch(array)))
+	{
+		endwin();
+		return (0);
+	}
 
 	int i = 0;
 	while (42)
 	{
-		ft_getch(array);
 		ft_draw(array);
+		if (!(ft_getch(array)))
+		{
+			endwin();
+			return (0);
+		}
 		i += 1;
 	}
 
